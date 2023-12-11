8e43dc446606f9e3ee118b7b812deae72be4aebc
AHaussG <andres.haussmann@usach.cl>
2023-12-11 19:59:22 -0300

    Se agrega proyecto de desarrollo de la evaluación 3

---
+++ PEP3_Mingeso/src/main/java/com/example/PEP3_Mingeso/Pep3MingesoApplication.java
@@ -0,0 +1,13 @@
+package com.example.PEP3_Mingeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pep3MingesoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pep3MingesoApplication.class, args);
+	}
+
+}
---
+++ PEP3_Mingeso/src/test/java/com/example/PEP3_Mingeso/Pep3MingesoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PEP3_Mingeso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pep3MingesoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
